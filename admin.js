f957097c27d6e90d34e580ddd59f1543186d41cb
wuxingsheng <wuxingsheng@qutoutiao.net>
2019-08-12 10:34:21 +0800

    components add done

--- admin.js
+++ admin.js
@@ -1,21 +1,17 @@
 var express = require('express');
+let GLOBAL = global;//全局变量
+let Modules = [];//所有模块名称集合
+GLOBAL.APP = express();//express
 
-const formidableMiddleware = require('express-formidable');
-
-let GLOBAL = global;
- 
-GLOBAL.APP = express()
+const formidableMiddleware = require('express-formidable');//接收表单数据中间件 req.files | 文件 req.fileds | 字段
 APP.use(formidableMiddleware());
-var cookieSession = require('cookie-session')
-APP.set('trust proxy', 1) // trust first proxy
 
-APP.use(cookieSession({
-  name: 'session',
-  keys: ['key1']
-}))
+//数据库连接
+let { getDb, closeDb } =  require('./db');
+GLOBAL.connection = getDb();//数据库连接池
 
 
-
+//设置跨域名
 APP.all('*', function(req, res, next) {  
   res.header("Access-Control-Allow-Origin", "*");  
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");  
@@ -25,14 +21,29 @@ APP.all('*', function(req, res, next) {
   else  next();  
 });
 
-let { getDb, closeDb } =  require('./db');
-let Modules = [];
-GLOBAL.connection = getDb();//数据库连接池
+//设置session
+var session = require('express-session');
+var sess = {
+  secret: 'keyboard cat',
+  cookie: {
+   // maxAge:1000 * 3
+  }
+}
+
+if (APP.get('env') === 'production') {
+  APP.set('trust proxy', 1) // trust first proxy
+  sess.cookie.secure = true // serve secure cookies
+}
+
+
+APP.use(session(sess));
+
+
 
 Modules.push(require('./modules/login').modulesId);//用户模块
 Modules.push(require('./modules/user').modulesId);//登录模块
 
-
+//启动服务
 var server = APP.listen(8081, function () {
 
   var host = server.address().address
@@ -42,7 +53,7 @@ var server = APP.listen(8081, function () {
 
 })
 
-
+//打印所有模块
 console.log('modulesId',Modules);
  
 
--- modules/login.js
+++ modules/login.js
@@ -1,25 +1,19 @@
 let modulesId = 'login';
- 
+ //uuid
+const uuidv1 = require('uuid/v1');
+
+
+
  //get-user 获取用户
  APP.post('/login-user', function (req, res) {
-
-  // console.log('req',typeof(req.fields));
-  // console.log('reqfiles',req.files);
-
-
-
-  let {username,password} = req.fields;
-
  
-   
+  let {username,password} = req.fields;
+ 
   let sql = `select * from user where username = '${username}'  and password_hash = '${password}'`;
-  
   connection.query(sql, function (error, results, fields) {
     if (error) throw error;
     if(results.length>0){
- 
-
-      let result = `登录成功 ${results[0].username}. ${cookieSession.isNew} `
+      let result = `登录成功 ${results[0].username}`
       res.send(result);
     }else{
       let result = `用户名或密码错误： ${username} .  ${password}`
@@ -32,11 +26,18 @@ let modulesId = 'login';
 
 // add-user  增加用户
 APP.get('/logout-user', function (req, res) {
-  // var n = req.session.views || 0
-  // req.session.views = n++
-  res.send(req.session.views + ' views12345')
+  if (req.session.views) {
+    req.session.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>views: ' + req.session.views + '</p>')
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 'uuid: '+ uuidv1() + 's</p>')
+    res.end()
+  } else {
+    req.session.views = 1
+    res.end('welcome to the session demo. refresh!')
+  }
 }) 
 
-module.exports= {
+module.exports = {
   modulesId
 }
